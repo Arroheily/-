99a63120937bb9a2998fc1f04b5ff14b687e4774
寸晖 吴 <cunhui.wu@sail-fs.com>
2018-05-31 18:11:34 +0800

    update

--- pager.js
+++ pager.js
@@ -1,9 +1,14 @@
 (function() {
     var _options = {
-        default_word: "default hello"
-    },
-
+        default_word: "default hello",
+        default_pageCount: 10,
+        default_pre: "Pre",
+    }    
     var _plugin_api = {
+        generatePage:function(id, count){
+            count = _options.default_pageCount;
+            pager_contain(id, count);
+        },
         firstFunc: function(str = _options.default_word) {
             alert(str);
             return this;
@@ -11,9 +16,18 @@
         secondFunc: function() {
             alert("secondFunc");
             return this;
+        },
+        pickerPage: function(){
+            alert(123)
+        }
+    }
+    var pager_contain = function(id, items){
+        var contain = document.getElementById(id);
+        for (var i = 0; i <= items; i++) {
+            contain.innerHTML  += '<span onclick="PGPlugin.pickerPage()">'+ i +'</span>';
         }
     }
 
-    this.CJPlugin = _plugin_api;
+    this.PGPlugin = _plugin_api;
 
 })();
