e5d78fd8f018dafed604c3c5aa8dc4401c37b8cd
寸晖 吴 <cunhui.wu@sail-fs.com>
2018-06-06 18:12:13 +0800

    html抽取拼写

--- pager.js
+++ pager.js
@@ -6,7 +6,9 @@
     }    
     var _plugin_api = {
         generatePage:function(id, count){
+            if(count == undefined){
             count = _options.default_pageCount;
+        }
             pager_contain(id, count);
         },
         firstFunc: function(str = _options.default_word) {
@@ -18,14 +20,59 @@
             return this;
         },
         pickerPage: function(){
-            alert(123)
+            // alert(event.target.id)
+            var lastPicker = event.target.id;
+            this.toggleClass(event.target,"abs-pager-active");
+
+        },
+
+        // 关于class的添加，删除
+        hasClass: function(obj, cls){            
+            return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+        },
+        addClass: function(obj, cls){
+            if (!this.hasClass(obj, cls)) obj.className += " " + cls;
+        },
+        removeClass: function(obj, cls){
+            if (this.hasClass(obj, cls)) {  
+                var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');  
+                obj.className = obj.className.replace(reg, ' ');  
+            }                
+        },
+        toggleClass: function(obj, cls){
+            if(this.hasClass(obj,cls)){  
+                this.removeClass(obj, cls);  
+            }else{  
+                this.addClass(obj, cls);  
+            }
+        }
+
+    }
+
+    var pager_contain = function(id, items){
+        var contain = document.getElementById(id);        
+        for (var i = 1; i <= items; i++) {
+            // contain.innerHTML  += '<span class="abs-pager-btn abs-pager-active" id="abs-pager-btn'+id+'_'+i+'" onclick="PGPlugin.pickerPage()">'+ i +'</span>';
+            contain.innerHTML  += pager_html("span",id,"abs-pager-btn abs-pager-active",'onclick="PGPlugin.pickerPage()"',i);
         }
     }
-    var pager_contain = function(id, items){
-        var contain = document.getElementById(id);
-        for (var i = 0; i <= items; i++) {
-            contain.innerHTML  += '<span onclick="PGPlugin.pickerPage()">'+ i +'</span>';
+    var pager_html = function(tag, id, classname, callback, content){
+        var complete_html = '<'+ tag +' ';
+        if(id != null){
+            complete_html += 'id="'+id+'" ';
         }
+        if(classname != null){
+            complete_html += 'class="'+ classname +'" ';
+        }
+        if(callback != null){
+            complete_html += callback;
+        }
+        complete_html += '>';
+        if(content != null){
+            complete_html += content;
+        }
+        complete_html += '</'+ tag +'>';
+        return complete_html;
     }
 
     this.PGPlugin = _plugin_api;
